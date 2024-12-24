10e8ec7b3dbe4c004fc70774992c78bc56d4e0e2
Zairth <mano.gimenez@hotmail.com>
2024-12-24 17:10:51 +0100

    g

--- main.h
+++ main.h
@@ -1,7 +1,9 @@
 #ifndef MAIN_H
 #define MAIN_H
+#include <unistd.h>
 
 extern char **environ;
 char **_getenv(void);
 char *get_the_right_path(char **argv, char **full_path);
+char **tokenize(ssize_t bytes_read, char *line);
 #endif
