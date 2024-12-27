12bf6c8ac156d331c32134a57408e5c48edec225
Zairth <mano.gimenez@hotmail.com>
2024-12-27 14:10:08 +0100

    Adding the new version of the simple shell, with the free problemes resolved

--- main.h
+++ main.h
@@ -3,8 +3,7 @@
 #include <unistd.h>
 
 extern char **environ;
-char **_getenv(void);
 char **tokenize(ssize_t bytes_read, char *line);
-char *get_the_right_path(char **argv, char **full_path);
-void full_free(char **full_path, char **argv, char *line, char *right_path);
+char **_getenv(void);
+char *get_the_right_path(char *argv, char **full_path);
 #endif
