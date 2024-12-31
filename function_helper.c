1c11b81e85fb987d69c97c731b154059784641ed
Zairth <mano.gimenez@hotmail.com>
2024-12-31 15:49:10 +0100

    Modifying the old _strcmp function because it was partially false

--- function_helper.c
+++ function_helper.c
@@ -39,32 +39,19 @@ int _strlen(char *s)
  *_strcmp - compare 2 different string variable
  *@s1: var that return the int
  *@s2: var to compare with the first var
- *Return: 0 if same, -1 if less, +1 if bigger
+ *Return: the difference or 0 if same
  */
 int _strcmp(char *s1, char *s2)
 {
 	int i = 0;
-	int value = 0;
-	int found_diff = 0;
 
 	while (s1[i] != '\0' && s2[i] != '\0')
 	{
-		if (found_diff == 0)
-		{
-			if (s1[i] > s2[i])
-			{
-				value = s1[i] - s2[i];
-				found_diff++;
-			}
-			else if (s1[i] < s2[i])
-			{
-				value = s1[i] - s2[i];
-				found_diff++;
-			}
-		}
+		if (s1[i] != s2[i])
+			return (s1[i] - s2[i]);
 		i++;
 	}
-	return (value);
+	return (s1[i] - s2[i]);
 }
 
 /**
